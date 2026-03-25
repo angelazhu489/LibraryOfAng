c9eb6005f0d934134e6fa3ee89c95ee49ef217c9
angelazhu489 <angelazhu489@gmail.com>
2026-03-24 18:39:22 -0700

    input validation

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;   // Schema constructor
 const bcrypt = require('bcrypt');
+const validator = require('validator')
 
 const userSchema = new Schema({   // create Schema
   email: {
@@ -16,10 +17,15 @@ const userSchema = new Schema({   // create Schema
 
 // static signup method 
 userSchema.statics.signup = async function (email, password) {
+  // input validation
+  if (!email || !password) throw Error('All fields must be filled')
+  if (!validator.isEmail(email)) throw Error('Email is not  valid')
+  if (!validator.isStrongPassword(password)) throw Error('Password not strong enough')
+
   // throw error if email exsts in db
   const exists = await this.findOne({ email })
   if (exists) {
-    throw Error('An account already exists with this email.')
+    throw Error('An account already exists with this email')
   }
   // hash password
   const saltRounds = 10;
