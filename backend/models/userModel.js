57755212a40ec582f534d9726a418f545efa3652
angelazhu489 <angelazhu489@gmail.com>
2026-03-24 18:21:09 -0700

    signup user/ add user to db

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;   // Schema constructor
+const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({   // create Schema
   email: {
@@ -12,6 +13,21 @@ const userSchema = new Schema({   // create Schema
     required: true
   }
 });
+
+// static signup method 
+userSchema.statics.signup = async function (email, password) {
+  // throw error if email exsts in db
+  const exists = await this.findOne({ email })
+  if (exists) {
+    throw Error('An account already exists with this email.')
+  }
+  // hash password
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds)
+  const hash = await bcrypt.hash(password, salt)
+  const user = await this.create({ email, password: hash })
+  return user;
+}
 
 const User = mongoose.model('User', userSchema);  // get collection
 
