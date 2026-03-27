7c6409be7c9b9dc813274bc1e12d656049bac9fb
angelazhu489 <angelazhu489@gmail.com>
2026-03-27 13:23:21 -0700

    fix file name

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,25 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 
 const Navbar = () => {
+  const logout = useLogout();
+
+  // logout button
+  const handleClick = () => {
+    logout();
+  }
+
   return (
     <header>
       <div className="container">
         <Link to="/">
           <h1 className="text-3xl inline hover:text-amber-600">Library of Ang</h1>
         </Link>
-        <nav>
-          <div>
-            <Link to="/about">About</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
-          </div>
+        <nav className="space-x-3">
+          <Link className="hover:cursor-pointer hover:text-amber-600" to="/about">About</Link>
+          <Link className="hover:cursor-pointer hover:text-amber-600" to="/login">Login</Link>
+          <Link className="hover:cursor-pointer hover:text-amber-600" to="/signup">Signup</Link>
+          <button onClick={handleClick} className="hover:cursor-pointer hover:text-amber-600">Logout</button>
         </nav>
       </div>
     </header>
